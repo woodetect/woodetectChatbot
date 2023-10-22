6d61f8e6e70e09f04bf6f573ae0b278f7ce1ec23
martin legrand <martin.legrand@epitech.eu>
2023-10-22 10:35:11 +0200

    Feat : fix missing new line stopping generation when send to stdin

--- backend_llama/interact.py
+++ backend_llama/interact.py
@@ -7,13 +7,12 @@ control = pyllamacpp()
 for word in control.next_word():
     if control.exited():
         break
-    if control.is_loading() and control.waiting():
-        control.send("###Human: Hello\n###Assistant: ")
     if control.is_loading():
         print("Waiting for llm...")
     if control.waiting() and word == "":
         question = input("\nEnter query: ")
-        formatted_question = f"###Human: {question}\n###Assistant:\n"
+        # no capital letters so it doesnt interfere with the stop keyword 
+        formatted_question = f"###human: {question} ###assistant:\n" # /!\ \n IS VERY IMPORTANT (OTHERWISE LLAMA WILL NOT RESPOND)
         control.send(formatted_question)
     print(word, end=' ')
     if '\n' in word:
